d178a628aaff1fd8da5426490571b3d1ef85d158
Sandra Hurtado <sandra.hurtado16@iesdoctorbalmis.com>
2020-10-19 12:10:54 +0200

    Agregar archivos de proyecto.

---
+++ Actividad4_LimitacionCaracteres/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Actividad4_LimitacionCaracteres
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void escribirTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int longitud = escribirTextBox.Text.Length;
+            if (longitud == 140)
+                escribirTextBox.IsReadOnly = true;
+        }
+    }
+}
